ba82abe1307707e70fa3c73515576170e4403bb2
Sheepy-9 <siposdominiktamas@proton.me>
2026-03-02 11:24:04 +0100

    GUI text fixed

--- mod/common/src/main/java/gjum/minecraft/mapsync/common/ModGui.java
+++ mod/common/src/main/java/gjum/minecraft/mapsync/common/ModGui.java
@@ -1,7 +1,6 @@
 package gjum.minecraft.mapsync.common;
 
 import gjum.minecraft.mapsync.common.config.ServerConfig;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.components.EditBox;
@@ -22,6 +21,9 @@ public class ModGui extends Screen {
 	int left;
 	int right;
 	int top;
+
+	int centerX = width / 2;
+	int centerY = width / 2;
 
 	EditBox syncServerAddressField;
 	Button syncServerConnectBtn;
@@ -46,8 +48,8 @@ public class ModGui extends Screen {
 			right = width / 2 + innerWidth / 2;
 			top = height / 3;
 
-            int centerX = width / 2;
-            int centerY = width / 2;
+            centerX = width / 2;
+            centerY = width / 2;
 
             int buttonWidth = 100;
             int buttonHeight = 20;
@@ -61,7 +63,6 @@ public class ModGui extends Screen {
 			);
 
 			if (serverConfig != null) {
-			    MapSyncMod.logger.info("serverConfig is not null");
 				addWidget(syncServerAddressField = new EditBox(font,
 						left,
 						top + 40,
@@ -73,10 +74,11 @@ public class ModGui extends Screen {
 
 				addRenderableWidget(
 					syncServerConnectBtn = Button.builder(Component.literal("Connect"), this::connectClicked)
-						.pos(right - 100, top + 40)
-						.width(100)
+						.bounds(right - 100, top + 40, 100, 20)
 						.build()
 				);
+
+				
 			}
 		} catch (Throwable e) {
 			e.printStackTrace();
@@ -98,16 +100,16 @@ public class ModGui extends Screen {
 
 	@Override
 	public void render(@NotNull GuiGraphics guiGraphics, int i, int j, float f) {
-        super.render(guiGraphics, i, j, f);
-
+		
 		try {
 			// wait for init() to finish
 			if (syncServerAddressField == null) return;
 			if (syncServerConnectBtn == null) return;
+			super.render(guiGraphics, i, j, f);
 
-			//renderBackground(guiGraphics, i, j, f);
-			guiGraphics.drawCenteredString(font, title, width / 2, top, 0xFFFFFF);
+			guiGraphics.drawCenteredString(font, title, centerX, top, 0xFFFFFFFF);
 			syncServerAddressField.render(guiGraphics, i, j, f);
+			syncServerConnectBtn.render(guiGraphics, j, j, i);
 
 			var dimensionState = getMod().getDimensionState();
 			if (dimensionState != null) {
@@ -118,32 +120,30 @@ public class ModGui extends Screen {
 						dimensionState.getNumChunksRendered(),
 						dimensionState.getRenderQueueSize()
 				);
-				guiGraphics.drawString(font, counterText, left, top + 70, 0x888888);
+				guiGraphics.drawCenteredString(font, counterText, centerX, syncServerAddressField.getY() - 20, 0xFF888888);
 			}
 
 			int numConnected = 0;
-			int msgY = top + 90;
+			int msgY = syncServerAddressField.getY() - 40;
 			var syncClients = getMod().getSyncClients();
 			for (var client : syncClients) {
 				int statusColor;
 				String statusText;
 				if (client.isEncrypted()) {
 					numConnected++;
-					statusColor = 0x008800;
+					statusColor = 0xFF008800;
 					statusText = "Connected";
 				} else if (client.getError() != null) {
-					statusColor = 0xff8888;
+					statusColor = 0xFFff8888;
 					statusText = client.getError();
 				} else {
-					statusColor = 0xffffff;
+					statusColor = 0xFFffffff;
 					statusText = "Connecting...";
 				}
 				statusText = client.address + "  " + statusText;
 				guiGraphics.drawString(font, statusText, left, msgY, statusColor);
 				msgY += 10;
 			}
-
-			super.render(guiGraphics, i, j, f);
 		} catch (Throwable e) {
 			e.printStackTrace();
 		}
